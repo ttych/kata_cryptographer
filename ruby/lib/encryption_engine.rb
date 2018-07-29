b73fa1259b2a2c18e915e9abf2aa07b55fc84bf2
ttych <thomas.tych@gmail.com>
2018-07-29 09:35:45 +0200

    [green] test_it_encrypts_lowercases_using_rot13_algorithm

--- ruby/lib/encryption_engine.rb
+++ ruby/lib/encryption_engine.rb
@@ -1,8 +1,21 @@
+require 'byebug'
+
 # EncryptionEngine to encrypt/decrypt messages
 # - use ROT13
 class EncryptionEngine
-  def encrypt(_message)
-    'Zl Zrffntr'
+  LOWERCASES = ('a'..'z').to_a
+
+  ROT13_SHIFT = 13
+
+  def encrypt(message)
+    message.chars.map do |char|
+      if LOWERCASES.include?(char)
+        new_char_ord = (LOWERCASES.index(char) + 13) % LOWERCASES.size
+        LOWERCASES[new_char_ord]
+      else
+        char
+      end
+    end.join
   end
 
   def decrypt(_message)
--- ruby/test/encryption_engine_test.rb
+++ ruby/test/encryption_engine_test.rb
@@ -3,10 +3,24 @@ require 'encryption_engine'
 
 # EncryptionEngine tests, using ROT13 algorithm
 class EncryptionEngineTest < Minitest::Test
+  def test_it_encrypts_lowercases_using_rot13_algorithm
+    engine = EncryptionEngine.new
+    output = engine.encrypt('my message')
+    assert_equal 'zl zrffntr', output
+  end
+
   def test_it_encrypts_using_rot13_algorithm
+    skip
     engine = EncryptionEngine.new
     output = engine.encrypt('My Message')
     assert_equal 'Zl Zrffntr', output
+  end
+
+  def test_it_encrypts_a_complex_message_using_rot13_algorithm
+    skip
+    engine = EncryptionEngine.new
+    output = engine.encrypt('This is my secret')
+    assert_equal 'Guvf vf zl frperg', output
   end
 
   def test_it_decrypts_using_rot13_algorithm
