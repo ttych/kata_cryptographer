79fc718312de9efa0e98badfb8c67d835ba64982
ttych <thomas.tych@gmail.com>
2018-07-29 09:45:19 +0200

    [refacto] extract method _do_rot

--- ruby/lib/encryption_engine.rb
+++ ruby/lib/encryption_engine.rb
@@ -10,19 +10,25 @@ class EncryptionEngine
 
   def encrypt(message)
     message.chars.map do |char|
-      if LOWERCASES.include?(char)
-        new_char_ord = (LOWERCASES.index(char) + 13) % LOWERCASES.size
-        LOWERCASES[new_char_ord]
-      elsif UPPERCASES.include?(char)
-        new_char_ord = (UPPERCASES.index(char) + 13) % UPPERCASES.size
-        UPPERCASES[new_char_ord]
-      else
-        char
-      end
+      _do_rot(char)
     end.join
   end
 
   def decrypt(_message)
     'My Message'
+  end
+
+  private
+
+  def _do_rot(char)
+    if LOWERCASES.include?(char)
+      new_char_ord = (LOWERCASES.index(char) + ROT13_SHIFT) % LOWERCASES.size
+      LOWERCASES[new_char_ord]
+    elsif UPPERCASES.include?(char)
+      new_char_ord = (UPPERCASES.index(char) + ROT13_SHIFT) % UPPERCASES.size
+      UPPERCASES[new_char_ord]
+    else
+      char
+    end
   end
 end
