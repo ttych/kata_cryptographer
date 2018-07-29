6ff1f5f17f0cbc032b845302ea5f3ff4f26b68de
ttych <thomas.tych@gmail.com>
2018-07-29 09:42:14 +0200

    [refacto] use setup to factorize engine instantiation

--- ruby/test/encryption_engine_test.rb
+++ ruby/test/encryption_engine_test.rb
@@ -3,33 +3,32 @@ require 'encryption_engine'
 
 # EncryptionEngine tests, using ROT13 algorithm
 class EncryptionEngineTest < Minitest::Test
+  def setup
+    @engine = EncryptionEngine.new
+  end
+
   def test_it_encrypts_lowercases_using_rot13_algorithm
-    engine = EncryptionEngine.new
-    output = engine.encrypt('my message')
+    output = @engine.encrypt('my message')
     assert_equal 'zl zrffntr', output
   end
 
   def test_it_encrypts_uppercases_using_rot13_algorithm
-    engine = EncryptionEngine.new
-    output = engine.encrypt('MY MESSAGE')
+    output = @engine.encrypt('MY MESSAGE')
     assert_equal 'ZL ZRFFNTR', output
   end
 
   def test_it_encrypts_using_rot13_algorithm
-    engine = EncryptionEngine.new
-    output = engine.encrypt('My Message')
+    output = @engine.encrypt('My Message')
     assert_equal 'Zl Zrffntr', output
   end
 
   def test_it_encrypts_a_complex_message_using_rot13_algorithm
-    engine = EncryptionEngine.new
-    output = engine.encrypt('This is my secret')
+    output = @engine.encrypt('This is my secret')
     assert_equal 'Guvf vf zl frperg', output
   end
 
   def test_it_decrypts_using_rot13_algorithm
-    engine = EncryptionEngine.new
-    output = engine.decrypt('Zl Zrffntr')
+    output = @engine.decrypt('Zl Zrffntr')
     assert_equal 'My Message', output
   end
 end
